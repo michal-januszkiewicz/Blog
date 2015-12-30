ee43c412634793d8500d38f1d22bd81eb9afe900
michal_januszkiewicz <mljc92@gmail.com>
2015-12-30 13:44:16 +0100

    edit infos

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,5 +1,6 @@
 class InfosController < ApplicationController
   before_action :require_admin,   only: [:new, :edit, :create, :update, :destroy]
+  before_action :current_info,    only: [:edit, :update, :destroy]
 
   def index
     @infos = Info.all.order('created_at DESC')
@@ -10,10 +11,10 @@ class InfosController < ApplicationController
   end
 
   def edit
+    @info = Info.find(params[:id])
   end
 
   def create
-    @info = Info.new(info_params)
     if @info.save
       flash[:success] = t(:info_created)
       redirect_to infos_url
@@ -32,7 +33,7 @@ class InfosController < ApplicationController
   end
 
   def destroy
-    Info.find(params[:id]).destroy
+    @info.destroy
     flash[:success] = t(:info_deleted)
     redirect_to infos_url
   end
@@ -41,5 +42,9 @@ class InfosController < ApplicationController
 
     def info_params
       params.require(:info).permit(:title, :text)
+    end
+
+    def current_info
+      @info = Info.find(params[:id])
     end
 end
